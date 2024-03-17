a697a991b3b959a0baf51b71b3c1aac2233d6466
Suyash (Macbook Air) <suyash.lawand@gmail.com>
2024-03-18 00:12:23 +0530

    add: xUnit init

---
+++ 09-xUnit-Demo/xUnit-Demo/CRUDTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CRUDTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        
+    }
+}
---
+++ 09-xUnit-Demo/xUnit-Demo/ServiceContracts/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ServiceContracts;
+
+public class Class1
+{
+
+}
