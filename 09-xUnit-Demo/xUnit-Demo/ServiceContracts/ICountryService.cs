40b33881a29be941f9c0e4f163dd9ed5d987b5e2
Suyash (Macbook Air) <suyash.lawand@gmail.com>
2024-03-21 20:20:56 +0530

    add: xunit

---
+++ 09-xUnit-Demo/xUnit-Demo/ServiceContracts/ICountryService.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using ServiceContracts.DTO;
+
+namespace ServiceContracts;
+
+
+// <summary>Countains Country Service Contract logic</summary>
+public interface ICountryService
+{
+    public CountryAddResponse AddCountry(CountryAddRequest? countryAddRequest);
+    
+    
+}
---
+++ 09-xUnit-Demo/xUnit-Demo/Services/CountryService.cs
@@ -0,0 +1,12 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    public CountryAddResponse AddCountry(CountryAddRequest? countryAddRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
